a5c808bd4182ec447db0a0ca82398030c3dd491b
vadim <vadim.chursin.tgn@gmail.com>
2017-10-30 14:47:13 +0300

    middleware

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function(req, res){
-    res.send('Hello World!1')
+const middleware = function (req, res, next) {
+	console.log(req.path);
+
+	req['body'] = 'Hello World!';
+
+	next();
+}
+
+//app.use(middleware);
+app.get('/', middleware, function(req, res){
+    res.send(req.body);
 });
 
 app.post('/', function(req, res){
     res.send('Got a POST request')
 });
 
-app.listen(3002,function() {
+app.listen(3005,function() {
     console.log('Example app listening on port 3002!');
 });
 
